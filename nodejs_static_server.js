b466378a7ae2a64e8219181af7fddbc55c596b65
Bowen Zhong <bz4e14@soton.ac.uk>
2017-03-29 15:33:19 +0800

    nodejs static server v2

--- nodejs_static_server.js
+++ nodejs_static_server.js
@@ -1,75 +1,65 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+var url = require('url');
+var port = process.argv[2] || 9000;
 
 var mimeType = {
-  '.ico': 'image/x-icon',
-  '.html': 'text/html',
-  '.js': 'text/javascript',
-  '.json': 'application/json',
-  '.css': 'text/css',
-  '.png': 'image/png',
-  '.jpg': 'image/jpeg',
-  '.wav': 'audio/wav',
-  '.mp3': 'audio/mpeg',
-  '.svg': 'image/svg+xml',
-  '.pdf': 'application/pdf',
-  '.doc': 'application/msword',
-  '.eot': 'appliaction/vnd.ms-fontobject',
-  '.ttf': 'aplication/font-sfnt'
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword',
+    '.eot': 'appliaction/vnd.ms-fontobject',
+    '.ttf': 'aplication/font-sfnt'
 };
 
 var server = http.createServer(function(req, res) {
-	console.log(req.method, req.url);
-	var url = path.parse(req.url);
-	var filePath = path.join(process.cwd(), url.dir, url.base);
-	var mime = mimeType[url.ext] ? mimeType[url.ext] : 'text/plain';
-	console.log(url);
-	console.log(filePath);
+    console.log(req.method, req.url);
 
-	fs.stat(filePath, function(err, stats) {
-		if (err) {
-			res.writeHead(404);
-			return res.end('404');
-		}
-		
-		if (stats.isDirectory()) {
-			fs.readFile(filePath + '/index.html', function(err, data) {
-				if (err) {
-					res.writeHead(404);
-					res.end();
-				}
-				res.writeHead(200, {
-					'Content-Type': 'text/html'
-				});
-				res.write(data);
-				res.end();
-			});
-		} else {
-			fs.readFile(filePath, function(err, data) {
-				if (err) {
-					res.writeHead(404);
-					res.end();
-				}
+    var parsedUrl = url.parse(req.url);
+    var filePath = path.join(process.cwd(), parsedUrl.pathname);
+    var fileExt = path.parse(req.url).ext;
+    var mime = mimeType[fileExt] || 'text/plain';
+    console.log(parsedUrl);
+    console.log(filePath);
 
-				res.writeHead(200, {
-					'Content-Type': mime
-				});
-				res.write(data);
-				res.end();
-			});
-		}
-	});
 
-	
-});
+    fs.stat(filePath, function(err, stats) {
+        if (err) {
+            res.statusCode = 404;
+            res.end(`File ${filePath} not found`);
+            return;
+        }
 
-server.listen(8000, function() {
-	console.log('server is listening on port 8000');
+        if (stats.isDirectory()) {
+            filePath += '/index.html';
+        }
+
+        fs.readFile(filePath, function(err, data) {
+            if (err) {
+                res.statusCode = 500;
+                res.end(`Error getting the file: ${err}`);
+            } else {
+                res.writeHead(200, {
+                    'Content-Type': mime
+                });
+                res.write(data);
+                res.end();
+            }
+        });
+    });
 });
 
 
 
-
-
-
+server.listen(parseInt(port, 10), function() {
+    console.log(`server is listening on port ${parseInt(port, 10)}`);
+});

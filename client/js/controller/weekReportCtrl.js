7b37cf75227da613ddc955409637bc6a29092dc2
zbw322092 <bz4e14@soton.ac.uk>
2017-04-17 23:43:55 +0800

    add row functionality

--- client/js/controller/weekReportCtrl.js
+++ client/js/controller/weekReportCtrl.js
@@ -4,6 +4,28 @@ app.controller('WeekReportCtrl', [
 	$scope
 ) {
 	$scope.canIBind = 'Yes, you can';
+	$scope.formData = [{
+		affairs: null,
+		time: null,
+		author: null,
+		completion: null
+	}];
 
-	
+	$scope.addNewLine = function() {
+		var initRowData = {
+			affairs: null,
+			time: null,
+			author: null,
+			completion: null
+		};
+		$scope.formData.push(initRowData);
+	};
+
+	$scope.submitForm = function() {
+		console.log($scope.formData);
+	};
+
+
+
+
 }]);
